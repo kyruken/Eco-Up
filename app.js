d5d3e006461396c4bac7c6007153385dfd71ceeb
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-03 18:22:30 -0800

    add .env, add database connection

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err));
 
 const directoryRouter = require('./routes/directory');
 
