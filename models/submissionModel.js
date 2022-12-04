fd5448a893e41df863555623918cee5deecf72ed
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-03 20:06:25 -0800

    take out username for now

--- models/submissionModel.js
+++ models/submissionModel.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const submissionSchema = mongoose.Schema({
-    username: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-    },
+    //Will figure out how to connect a user with a submission later
+    // username: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     required: true
+    // },
     answer: {
         type: String,
         required: true
