ad733a360e7064cc5a053e01d587d03cb66d912d
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-12 10:20:54 -0800

    create logic for registering user

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const User = require('./models/userModel');
 const app = express();
@@ -14,7 +15,9 @@ mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', (err) => console.log(err));
 
-app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 const directoryRouter = require('./routes/directory');
 
 app.use(cors({
@@ -47,6 +50,30 @@ app.post('/login', (req, res, next) => {
                 res.send('Wrong password');
             }
         })
+    })
+})
+
+app.post('/register', (req, res) => {
+    User.findOne({username: req.username}, (err, theUser) => {
+        if (err) throw err;
+
+        if(theUser) res.send("User Exists Already");
+
+        if(!theUser) {
+            bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+                if (err) throw err;
+
+                const newUser = new User({
+                    username: req.body.username,
+                    password: hashedPassword,
+                    email: req.body.email,
+                    submissions: [],
+                })
+
+                newUser.save();
+                res.send("New user created");
+            })
+        }
     })
 })
 
